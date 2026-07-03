689f307b2c334bb755cd7dc88665c7185b80eecf
Sunil Ghate <05510@sdloffice.com>
2026-07-03 12:51:27 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost/sdlhub-backend",
+        target: "http://localhost:8080/sdlhub-backend",
         changeOrigin: true,
       },
     },
