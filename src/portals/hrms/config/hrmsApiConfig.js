730ae047434b2fb49802b514d54c45f94d27042e
Sunil Ghate <05510@sdloffice.com>
2026-08-10 10:00:27 +0530

    conflict resolved

--- src/portals/hrms/config/hrmsApiConfig.js
+++ src/portals/hrms/config/hrmsApiConfig.js
@@ -4,55 +4,31 @@ export const HRMS_API = {
   },
 
   MAINTAINANCE: {
-<<<<<<< HEAD
-    KRAACTIVITY_GET: "/maintainance/kraactivity/getKRAActivityList.php",
-    KRAMASTER: "/maintainance/kraactivity/getKRAMaster.php",
-    KRAACTIVITY_SAVE: "/maintainance/kraactivity/saveKRAActivity.php",
-    KRAACTIVITY_DELETE: "/maintainance/kraactivity/deleteKRAActivity.php",
-    
-    QUESTION_GET: "/maintainance/question/getQuestionList.php",
-    QUESTION_SAVE: "/maintainance/question/saveQuestion.php",
-    QUESTION_DELETE: "/maintainance/question/deleteQuestion.php",
-    QUESTION_GROUPS: "/maintainance/question/getQuestionGroups.php",
-    QUESTION_SUBGROUPS: "/maintainance/question/getQuestionSubGroups.php",
-    
     CAPABILITIES_GET: "/maintainance/capabilities/getCapabilitiesList.php",
     CAPABILITIES_SAVE: "/maintainance/capabilities/saveCapabilities.php",
-
     DEPTACTIVITY_DEPTMST_GET: "/maintainance/deptactivities/getDepartmentMaster.php",
     DEPTACTIVITY_FETCH: "/maintainance/deptactivities/getDeptActivitiesList.php",
     DEPTACTIVITY_SAVE: "/maintainance/deptactivities/saveDeptActivities.php",
     DEPTACTIVITY_DELETE: "/maintainance/deptactivities/deleteDeptActivities.php",
-    DEPARTMENT: "/department/getDepartments.php",
-
     POLICY_GET: "/maintainance/policylist/getPoliciesList.php",
     COMPANY_MASTER_GET: "/maintainance/policylist/getCompanyMaster.php",
     DEPARTMENT_MASTER_GET: "/maintainance/policylist/getDepartmentMaster.php",
     DIVISION_MASTER_GET: "/maintainance/policylist/getDivisionMaster.php",
     POLICY_SAVE: "/maintainance/policylist/savePolicies.php",
-    POLICY_PUBLISH: "/maintainance/policylist/getPolicyPublish.php",
-        
-    GET_PROFILES: "/maintainance/profilemaintenance/getProfiles.php",
-    GET_PROFILE_ACCESS: "/maintainance/profilemaintenance/getProfileAccess.php",
-    SAVE_PROFILE_ACCESS: "/maintainance/profilemaintenance/saveProfileAccess.php",
-=======
-    KRAACTIVITY_GET: "/hrms/maintainance/kraactivity/getKRAActivityList.php",
-    KRAMASTER: "/hrms/maintainance/kraactivity/getKRAMaster.php",
-    KRAACTIVITY_SAVE: "/hrms/maintainance/kraactivity/saveKRAActivity.php",
-    KRAACTIVITY_DELETE: "/hrms/maintainance/kraactivity/deleteKRAActivity.php",
-    QUESTION_GET: "/hrms/maintainance/question/getQuestionList.php",
-    QUESTION_SAVE: "/hrms/maintainance/question/saveQuestion.php",
-    QUESTION_DELETE: "/hrms/maintainance/question/deleteQuestion.php",
-    QUESTION_GROUPS: "/hrms/maintainance/question/getQuestionGroups.php",
-    QUESTION_SUBGROUPS: "/hrms/maintainance/question/getQuestionSubGroups.php",
-
+    POLICY_PUBLISH: "/maintainance/policylist/getPolicyPublish.php",    
+    KRAACTIVITY_GET: "/maintainance/kraactivity/getKRAActivityList.php",
+    KRAMASTER: "/maintainance/kraactivity/getKRAMaster.php",
+    KRAACTIVITY_SAVE: "/maintainance/kraactivity/saveKRAActivity.php",
+    KRAACTIVITY_DELETE: "/maintainance/kraactivity/deleteKRAActivity.php",
+    QUESTION_GET: "/maintainance/question/getQuestionList.php",
+    QUESTION_SAVE: "/maintainance/question/saveQuestion.php",
+    QUESTION_DELETE: "/maintainance/question/deleteQuestion.php",
+    QUESTION_GROUPS: "/maintainance/question/getQuestionGroups.php",
+    QUESTION_SUBGROUPS: "/maintainance/question/getQuestionSubGroups.php",
     DEPARTMENT: "/department/getDepartments.php",
-
     GET_PROFILES: "/maintainance/profilemaintenance/getProfiles.php",
     GET_PROFILE_ACCESS: "/maintainance/profilemaintenance/getProfileAccess.php",
-    SAVE_PROFILE_ACCESS: "/maintainance/profilemaintenance/saveProfileAccess.php",
-      
->>>>>>> 8d9e016c05865a03bffe0163eaa932d85d9375aa
+    SAVE_PROFILE_ACCESS: "/maintainance/profilemaintenance/saveProfileAccess.php",      
   },
 
   DASHBOARD: {
@@ -64,8 +40,4 @@ export const HRMS_API = {
     GET_MASTER_TABLES: "/masterdata/master/getMasterTables.php",
     SAVE_MASTER_DATA: "/masterdata/master/saveMasterData.php",
   },
-<<<<<<< HEAD
-=======
-
->>>>>>> 8d9e016c05865a03bffe0163eaa932d85d9375aa
 };
