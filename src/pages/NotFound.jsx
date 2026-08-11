e3705877a4569e4d3e390bbc4db2708ddb8b8df7
Savita Jagtap <savita.jagtap@teamsdl.in>
2026-08-11 16:02:50 +0530

    Profile maintainance

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { IMAGES } from "../assets/assets"; 
+import { useLocation } from "react-router-dom";
+import { getPortalFromPath } from "../config/portalConfig";
 
 const NotFound = () => {
+
+   /* ==========================================================
+        PORTAL
+    ========================================================== */
+    const location = useLocation();
+    const portal = getPortalFromPath(location.pathname);
+    const portalHome = `/${portal.key}/dashboard`;
+
   return (
     <>
       <div class="error-box">
@@ -11,7 +21,7 @@ const NotFound = () => {
 				<h3 class="h2 mb-3">Oops, something went wrong</h3>
 				<p>Error 404 Page not found. Sorry the page you looking for
                     doesn’t exist or has been moved</p>
-         <Link to="/eportal/dashboard"  class="btn btn-primary">
+         <Link to={portalHome}  class="btn btn-primary">
             Go to Dashboard
           </Link>
 			</div>
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost/projects/sdlhub-backend",
+        target: "http://localhost/sdlhub-backend",
         changeOrigin: true,
       },
     },
